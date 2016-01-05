9a3d51fa6303b83b4f090092a8418451ec501989
Mariko Fujita <fujitam@umich.edu>
2016-01-05 11:29:22 -0500

    variables.R

---
+++ variables.R
@@ -0,0 +1,9 @@
+#This is the document where I learn introductory R.  
+x <- 1    #this doesn't execute
+y <- x*2 #so that you know what you did
+z <- y^2
+x+y+z
+#start lines with hashes
+answer<-TRUE
+height<-5.5
+dog_name<-"Charlie"
