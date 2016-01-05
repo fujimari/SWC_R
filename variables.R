fbe09711439e5701f1ab2007b19b9ab81b3640a8
Mariko Fujita <fujitam@umich.edu>
2016-01-05 11:43:10 -0500

    I am almost lose with git!

--- variables.R
+++ variables.R
@@ -1,4 +1,4 @@
-#This is the document where I learn introductory R.  
+#This is the document where I learn introductory R. January 5, 2016 
 x <- 1    #this doesn't execute
 y <- x*2 #so that you know what you did
 z <- y^2
